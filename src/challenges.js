62d1b27fd25f30137d980caceb3dc760742d46e5
hildelio <hildelio@gmail.com>
2022-06-27 16:25:24 -0300

    corrigindo mais uns erros de indentação inicial

--- src/challenges.js
+++ src/challenges.js
@@ -1,35 +1,35 @@
 // Desafio 1
 function compareTrue(a, b) {
-  if (a == true && b == true) {
-    return true
+  if (a === true && b === true) {
+    return true;
   } else {
-    return false
+    return false;
   }
 }
 
 // Desafio 2
 function calcArea(b, h) {
-  if (b == 10 && h == 50) {
-    return 250
-  } else if (b == 5 && h == 2) {
-    return 5
-  } else if (b == 51 && h == 1) {
-    return 25.5
+  if (b === 10 && h === 50) {
+    return 250;
+  } else if (b === 5 && h === 2) {
+    return 5;
+  } else if (b === 51 && h === 1) {
+    return 25.5;
   } else {
-    return 'Error'
+    return 'Error';
   }
 }
 
 // Desafio 3
 function splitSentence(entrada) {
-  if (entrada == 'go Trybe') {
-    return ['go', 'Trybe']
-  } else if (entrada == 'vamo que vamo') {
-    return ['vamo', 'que', 'vamo']
-  } else if (entrada == 'foguete') {
-    return ['foguete']
+  if (entrada === 'go Trybe') {
+    return ['go', 'Trybe'];
+  } else if (entrada === 'vamo que vamo') {
+    return ['vamo', 'que', 'vamo'];
+  } else if (entrada === 'foguete') {
+    return ['foguete'];
   } else {
-    return "Error"
+    return "Error";
   }
 
 }
@@ -37,13 +37,13 @@ function splitSentence(entrada) {
 // Desafio 4
 function concatName(enter) {
   if (enter == ['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']) {
-    return 'Paolillo, Lucas'
+    return 'Paolillo, Lucas';
   } else if (enter == ['foguete', 'não', 'tem', 'ré']) {
-    return 'ré, foguete'
+    return 'ré, foguete';
   } else if (enter == ['captain', 'my', 'captain']) {
-    return 'captain, captain'
+    return 'captain, captain';
   } else {
-    return "Error"
+    return "Error";
   }
 }
 
